ff4614ff9769047c5819030b29416253be2a3d5f
Evan BOISSONNOT <evan@boissonnot.fr>
2024-10-29 09:58:00 +0100

    chore: vite and razor page are configured

---
+++ spas/spa-with-vs/reactproject1/LudoTecks/App_Code/ViteManifestService.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace LudoTecks.App_Code
+{
+    public class ViteManifestService
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly string _manifestPath;
+
+        public ViteManifestService(IWebHostEnvironment env)
+        {
+            this._env = env;
+            this._manifestPath = Path.Combine(this._env.WebRootPath, "dist", "manifest.json");
+        }
+
+        public string GetAssetPath(string entry)
+        {
+            if (!this._env.IsProduction())
+            {
+                return $"http://localhost:5173/{entry}"; // Chemin pour le dev server de Vite
+            }
+
+            if (!File.Exists(this._manifestPath))
+                throw new FileNotFoundException("Manifest file not found", this._manifestPath);
+
+            var manifestJson = File.ReadAllText(this._manifestPath);
+            var manifest = JsonSerializer.Deserialize<Dictionary<string, ManifestEntry>>(manifestJson);
+
+            return manifest != null && manifest.TryGetValue(entry, out var entryData)
+                ? $"/dist/{entryData.file}"
+                : throw new Exception($"Entry {entry} not found in manifest");
+        }
+
+        private class ManifestEntry
+        {
+            public string file { get; set; }
+        }
+    }
+}
---
+++ spas/spa-with-vs/reactproject1/LudoTecks/Pages/Films/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LudoTecks.Pages.Films
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
