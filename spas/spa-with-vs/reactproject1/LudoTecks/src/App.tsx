46410a4e62f525fabc0dca38ab3d152d6f3410c5
Evan BOISSONNOT <evan@boissonnot.fr>
2024-10-29 10:53:09 +0100

    choe: everything is running !

---
+++ spas/spa-with-vs/reactproject1/LudoTecks/src/App.tsx
@@ -0,0 +1,18 @@
+﻿import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { Home } from "./Pages/Home";
+
+const routes = createRoutesFromElements(
+    <>
+        <Route path="" element={<Home></Home>}></Route>
+    </>
+  );
+
+const router = createBrowserRouter(
+    routes
+)
+
+export const App = () => {
+    return (
+        <RouterProvider router={router}></RouterProvider>
+    );
+}
